de848694d66912612d52397d8714d6c6c45e5803
aleksandar-code <83082486+aleksandar-code@users.noreply.github.com>
2023-01-29 17:12:38 +0100

    Add user_param

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,12 +3,16 @@ class UsersController < ApplicationController
     end
 
     def create
-        @user = User.new(username: params[:username], email: params[:email], password: params[:password])
-
+        # @user = User.new(username: params[:username], email: params[:email], password: params[:password])
+        @user = User.new(user_params)
         if @user.save
           redirect_to new_users_path
         else
           render :new, status: :unprocessable_entity
         end
+    end
+
+    def user_params
+        params.require(:user).permit(:username,:password,:email)
     end
 end
